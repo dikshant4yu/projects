6a6803a6bd1a9de5ad944fb2005962546133e675
dikshant4yu <dikshantclownprince@gmail.com>
2024-03-31 14:44:20 +0530

    Second commit

---
+++ accordion/src/components/Accordian/data.js
@@ -0,0 +1,19 @@
+const data = [
+    { 
+        id : "1",
+        question : "who are you?",
+        answer : "ting ling ka raja"
+    },
+    { 
+        id : "2",
+        question : "What you think of games and movies?",
+        answer : "They are more fun than my GF."
+    },
+    { 
+        id : "3",
+        question : "How you enjoy on weekends?",
+        answer : "GGGGAAAAMMMMEEEESSS!!!!!!"
+    }
+];
+
+export default data;
---
+++ accordion/src/components/Accordian/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import data from "./data";
+
+export default function Accordian() {
+    const [selected,setSelected] = useState(null);
+
+    function handleSingleSelection(currentID){
+        setSelected(selected === currentID ? null : currentID);
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="accordian">
+                    {data && data.length > 0 ? (
+                        data.map((dataItem) => ( 
+                        <div className="item">
+                            <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {
+                                selected === dataItem.id ?
+                                <div className="content">
+                                    {dataItem.answer}
+                                </div> 
+                                : null
+                            }
+                        </div>
+                    ) )
+                    )
+                    : (<div>No data found</div>)
+                    }
+            </div>
+        </div>
+    )
+}
